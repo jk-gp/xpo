9f2d71610b417e856c93d5c96f3715b7507c715d
jk-gp <jakub.kaminski@genpact.com>
2026-01-30 13:39:05 +0100

    Update Hub MSI URL for AirwatchAgent

--- ws1Setup.ps1
+++ ws1Setup.ps1
@@ -17,7 +17,7 @@ $GroupId        = "DataTechAILandingO"         # Your OG Group ID
 $ServerFqdn = ($ServerUrlInput -replace '^https?://','').TrimEnd('/')
 
 # Public Hub MSI (swap to your DS /agents path if you prefer)
-$HubMsiUrl = "https://packages.vmware.com/wsone/AirwatchAgent.msi"
+$HubMsiUrl = "https://packages.vmware.com/omnissa/AirwatchAgent.msi"
 
 # Ensure we are running elevated
 $currUser = [Security.Principal.WindowsIdentity]::GetCurrent()
